d0e33239106668d716031611d29157559cb729ca
Tony <tonytony771111@gmail.com>
2022-06-27 09:38:00 +0800

    [Feature] create ScreenScope

---
+++ lib/scope_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'fluttin_scope_component.dart';
+
+class ScreenScope extends FluttinScopeComponent {
+  
+}
+
+mixin ScreenScopeMixin<T extends StatefulWidget> on State<T> {
+  final ScreenScope _screenScope = ScreenScope();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _screenScope.closeScope();
+  }
+}
